722279448432acbdaf21b6f9c1fe33b424d5c55e
YONGJIE FU <yf2578@columbia.edu>
2022-01-08 03:16:52 +0000

    lwr

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from src.models.flow import RealNVP
 from src.utils import set_logger, delete_file_or_folder
 from src.training import training
 from src.dataset.arz_data import arz_data_loader
+from src.dataset.lwr_data import lwr_data_loader
 
 # CUDA support 
 if torch.cuda.is_available():
@@ -23,9 +24,8 @@ else:
     logging.info("cuda is not available")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--experiment_dir', default='experiments/arz',
+parser.add_argument('--experiment_dir', default='experiments/lwr',
                     help="Directory containing experiment_setting.json")
-parser.add_argument('--data_dir', default='data/arz', help="Directory containing the dataset")
 parser.add_argument('--restore_from', default=None,
                     help="Optional, file location containing weights to reload")
 parser.add_argument('--mode', default='train',
@@ -67,8 +67,15 @@ if __name__ == "__main__":
 
     # Create the input data pipeline
     logging.info("Loading the datasets...")
-    arz_data = arz_data_loader(params.data_arz['loop_number'],params.data_arz['noise_scale'],params.data_arz['noise_number'])
-    train_feature, train_label = arz_data.load_data()
+
+    # arz_data = arz_data_loader(params.data_arz['loop_number'],params.data_arz['noise_scale'],params.data_arz['noise_number'])
+    # train_feature, train_label = arz_data.load_data()
+    if params.data['type'] == 'lwr':
+        data_loaded = lwr_data_loader(params.data['loop_number'],params.data['noise_scale'],params.data['noise_number'])
+    elif params.data['type'] == 'arz':
+        data_loaded = arz_data_loader(params.data['loop_number'],params.data['noise_scale'],params.data['noise_number'])
+    train_feature, train_label = data_loaded.load_data()
+    logging.info("load data: " + f"{params.data['type']}")
     logging.info("train feature shape: " + f"{train_feature.shape}")
     logging.info("train label shape: " + f"{train_label.shape}")
     logging.info("- done.")
--- src/dataset/lwr_data.py
+++ src/dataset/lwr_data.py
@@ -1,97 +1,90 @@
 import scipy.io
 import numpy as np
 
-X_dim = 1 
-Y_dim = 1
-T_dim = 1
-Z_dim = 1
 
-# Noise level (in this noise free case is zero)
-noise = 0.02
-N_noise = 20 
-N_u = 100000
-N_loop = 8
+class lwr_data_loader():
 
-data = scipy.io.loadmat('rho_bellshape_10grid_DS10_gn_eps005_solver2_ring.mat')
+    def __init__(self, Loop_number, Noise_scale, Noise_number):
 
-t = data['t'].flatten()[:,None]# 960 by 1
-x = data['x'].flatten()[:,None]# 240 by 1
-Exact_rho = np.real(data['rho']).T
+        self.noise = Noise_scale
+        self.N_noise = Noise_number 
+        self.N_u = 100000
+        self.N_loop = Loop_number
+        self.miu = 1
+        self.sigma = 0.3 # for generating gaussion noise
 
-print(len(x), len(x[0]))
-print(N_loop-1)
+    def load_data(self):
+        data = scipy.io.loadmat('data/lwr/rho_bellshape_10grid_DS10_gn_eps005_solver2_ring.mat')
 
-
-X, T = np.meshgrid(x,t) #
-
-X_repeat = X.flatten()[:,None].repeat(N_noise, axis=1)
-T_repeat = T.flatten()[:,None].repeat(N_noise, axis=1)
-# all points inside
-X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None])) # hstack is column wise stack, 960*240 by 2
-index_x = np.where((X.flatten()[:,None] >= 0.4) & (X.flatten()[:,None] <=0.6))
-index_t = np.where((T.flatten()[:,None] >= 0) & (T.flatten()[:,None] <=0.5))
-print(index_x[0].shape,index_t[0].shape)
-index_shock_wave = np.intersect1d(index_x[0], index_t[0])
-print(index_shock_wave)
-# data2 = np.array([0.0]*650)
-idx2 = np.random.choice(range(960), 650, replace=False)
-data2 = np.array([0.0]*960)
-idx2 = np.array(list(range(960))) #np.random.choice(range(960), 650, replace=False)
-# t2 = t[idx].T#np.array(idx2).T
-
-# low boundary
-X_star2 = np.hstack((data2.flatten()[:,None], t.flatten()[:,None]))
-X_star2_repeat = np.repeat(X_star2,N_noise,axis = 0)
-
-idx3 = np.random.choice(range(960*241),  N_u, replace=False)
-X_star3 = X_star[idx3,:]
-X_star3_repeat = np.repeat(X_star3,N_noise,axis = 0)
+        t = data['t'].flatten()[:,None]# 960 by 1
+        x = data['x'].flatten()[:,None]# 240 by 1
+        Exact_rho = np.real(data['rho']).T
 
 
-rho_star = Exact_rho.flatten()[:,None] # 960*240 by 1
+        X, T = np.meshgrid(x,t) #
+
+        X_repeat = X.flatten()[:,None].repeat(self.N_noise, axis=1)
+        T_repeat = T.flatten()[:,None].repeat(self.N_noise, axis=1)
+        # all points inside
+        X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None])) # hstack is column wise stack, 960*240 by 2
+        # index_x = np.where((X.flatten()[:,None] >= 0.4) & (X.flatten()[:,None] <=0.6))
+        # index_t = np.where((T.flatten()[:,None] >= 0) & (T.flatten()[:,None] <=0.5))
+        # print(index_x[0].shape,index_t[0].shape)
+        # index_shock_wave = np.intersect1d(index_x[0], index_t[0])
+        # print(index_shock_wave)
+        # data2 = np.array([0.0]*650)
+        idx2 = np.random.choice(range(960), 650, replace=False)
+        data2 = np.array([0.0]*960)
+        idx2 = np.array(list(range(960))) #np.random.choice(range(960), 650, replace=False)
+        # t2 = t[idx].T#np.array(idx2).T
+
+        # low boundary
+        X_star2 = np.hstack((data2.flatten()[:,None], t.flatten()[:,None]))
+        X_star2_repeat = np.repeat(X_star2, self.N_noise,axis = 0)
+
+        idx3 = np.random.choice(range(960*241),  self.N_u, replace=False)
+        X_star3 = X_star[idx3,:]
+        X_star3_repeat = np.repeat(X_star3,self.N_noise,axis = 0)
 
 
-# Doman bounds
-lb = X_star.min(0) # [0, 0]
-ub = X_star.max(0) # [1, 3] 
+        rho_star = Exact_rho.flatten()[:,None] # 960*240 by 1
 
-print(lb)
-print(ub)
 
-gap = int(240.0/(N_loop-1))
-loopidx = [i*gap for i in range(N_loop-1)]
-if N_loop == 4: loopidx[-1] = 120
-loopidx.append(239)
-span = np.array(loopidx)
+        # Doman bounds
+        lb = X_star.min(0) # [0, 0]
+        ub = X_star.max(0) # [1, 3] 
 
-idx = []
-for i in range(960):
-    seg = list(span+i*241)
-    idx+= seg
+        gap = int(240.0/(self.N_loop-1))
+        loopidx = [i*gap for i in range(self.N_loop-1)]
+        if self.N_loop == 4: loopidx[-1] = 120
+        loopidx.append(239)
+        span = np.array(loopidx)
 
-print(len(idx), span)
-print(len(idx),'the number of record on loops')
-X_rho_train = X_star[idx,:]
-# add shcock wave
-# X_rho_shockwave = X_star[index_shock_wave,:]
-# X_rho_train = np.vstack((X_rho_train,X_rho_shockwave))
-##
-X_rho_repeat = np.repeat(X_rho_train,N_noise,axis = 0)
-print("X_rho_repeat:",X_rho_repeat.shape)
-rho_train = rho_star[idx,:]
-# add shcok wave
-# rho_train_shockwave = rho_star[index_shock_wave,:]
-# rho_train = np.vstack((rho_train,rho_train_shockwave))
-##
-print(rho_train.shape)
-# rho_train = rho_train + noise*np.random.randn(rho_train.shape[0], rho_train.shape[1])
-rho_train_repeat = rho_train + noise*np.random.randn(rho_train.shape[0], rho_train.shape[1])
-for i in range(N_noise-1):
-    rho_train_repeat = np.hstack((rho_train_repeat, rho_train + noise*np.random.randn(rho_train.shape[0], rho_train.shape[1])))
-rho_train_repeat = rho_train_repeat.reshape(-1,1)
-print("rho_train_repeat:",rho_train_repeat.shape)
+        idx = []
+        for i in range(960):
+            seg = list(span+i*241)
+            idx+= seg
 
-# Model creation
-layers_P = np.array([X_dim+T_dim+Z_dim,20,20,40,60,80,60,40,20,20,Y_dim])
-layers_Q = np.array([X_dim+T_dim+Y_dim,20,20,40,60,80,60,40,20,20,Z_dim])  
-layers_T = np.array([X_dim+T_dim+Y_dim,20,20,40,60,80,60,40,20,20,1])
+        X_rho_train = X_star[idx,:]
+        # add shcock wave
+        # X_rho_shockwave = X_star[index_shock_wave,:]
+        # X_rho_train = np.vstack((X_rho_train,X_rho_shockwave))
+        ##
+        X_rho_repeat = np.repeat(X_rho_train,self.N_noise ,axis = 0)
+        rho_train = rho_star[idx,:]
+        # add shcok wave
+        # rho_train_shockwave = rho_star[index_shock_wave,:]
+        # rho_train = np.vstack((rho_train,rho_train_shockwave))
+
+        # rho_train = rho_train + noise*np.random.randn(rho_train.shape[0], rho_train.shape[1])
+        rho_train_repeat = rho_train + self.noise *np.random.randn(rho_train.shape[0], rho_train.shape[1])
+        for i in range(self.N_noise -1):
+            rho_train_repeat = np.hstack((rho_train_repeat, rho_train + self.noise *np.random.randn(rho_train.shape[0], rho_train.shape[1])))
+        rho_train_repeat = rho_train_repeat.reshape(-1,1)
+        gaussion_noise = np.random.normal(self.miu,self.sigma,rho_train_repeat.shape[0]).reshape(-1,1)
+        rho_noisie_repeat  = np.concatenate((rho_train_repeat, gaussion_noise),axis=1)
+
+        return X_rho_repeat.astype(np.float32), rho_noisie_repeat.astype(np.float32)
+
+
+
